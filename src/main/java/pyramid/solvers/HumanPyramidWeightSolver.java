00b8e3dea0d28a36c8d4d15c849577f470eddee6
pledenev <pledenev@gmail.com>
2015-04-20 16:54:47 +0300

    + solvers interruption added

--- src/main/java/pyramid/solvers/HumanPyramidWeightSolver.java
+++ src/main/java/pyramid/solvers/HumanPyramidWeightSolver.java
@@ -1,8 +1,10 @@
 package pyramid.solvers;
 
-import org.apache.commons.math3.util.FastMath;
+import com.google.common.math.BigIntegerMath;
 import org.springframework.stereotype.Component;
-import pyramid.controllers.InternalServerFailure;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  * Created by ledenev.p on 20.04.2015.
@@ -23,27 +25,42 @@ public class HumanPyramidWeightSolver implements IHumanPyramidWeightSolver {
 
     @Override
     public Double computeHumanEdgeWeightOn(int level) {
-        return weight0 * (1 - 1 / Math.pow(2, level));
+
+        BigDecimal exponent = BigDecimal.valueOf(2).pow(level);
+
+        return weight0 * (BigDecimal.ONE.subtract(BigDecimal.ONE.divide(exponent)).doubleValue());
     }
 
     @Override
     public Double computeHumanIndexWeightOn(int level, int index) {
 
-        long combinationNumberSum = 0;
-        for (int i = 0; i < index; i++) {
+        BigInteger combinationsSum = BigInteger.ZERO;
+        for (int i = 0; i <= index; i++) {
 
-            if (Thread.interrupted())
-                throw new InternalServerFailure();
+            if (Thread.interrupted()) {
+                System.out.println("Computing combinations aborted");
+                throw new SolverInterruptedFailure();
+            }
 
-            System.out.println("Computing combinationNumber(" + (level + 2) + "," + i + ")");
-            combinationNumberSum += combinationNumber(level + 2, i) * (1 + index - i);
+            System.out.println("Computing combinations (" + (level + 2) + "," + i + ")");
+            BigInteger combinationNumber = combinations(level + 2, i);
+            //System.out.println("combinations: " + combinationNumber);
+
+            combinationsSum = combinationsSum.add(combinationNumber.multiply(BigInteger.valueOf(1 + index - i)));
         }
 
-        return (1 + 2 * index - combinationNumberSum / FastMath.pow(2, level)) * weight0;
+        BigDecimal decimalCombinationsSum = new BigDecimal(combinationsSum);
+        BigDecimal exponent = BigDecimal.valueOf(2).pow(level);
+
+        return (1 + 2 * index - decimalCombinationsSum.divide(exponent).doubleValue()) * weight0;
     }
 
-    private int combinationNumber(int n, int k) {
-        return 0;
+    private BigInteger combinations(int n, int k) {
+        BigInteger nF = BigIntegerMath.factorial(n);
+        BigInteger kF = BigIntegerMath.factorial(k);
+        BigInteger nkF = BigIntegerMath.factorial(n - k);
+
+        return nF.divide(kF.multiply(nkF));
     }
 
 
---
+++ src/main/java/pyramid/solvers/SolverInterruptedFailure.java
@@ -0,0 +1,7 @@
+package pyramid.solvers;
+
+/**
+ * Created by ledenev.p on 20.04.2015.
+ */
+public class SolverInterruptedFailure extends RuntimeException{
+}
